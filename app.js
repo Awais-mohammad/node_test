386fcdf04fe635ecb0b7c88f99f9d9f89d1ba37b
Muhammad Awais <64267313+Awais-mohammad@users.noreply.github.com>
2022-03-05 15:01:38 +0500

    updated!!!

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+// dependencies
+// const express = require('express')
+// const app = express()
+// const Test = require('./modules/test.controller')
+
+// // creating route
+// app.get('/home', (req, res) => {
+
+//     result = Test.solution();
+//     res.end('welcome ',res)
+// })
+
+// app.listen(5000)
+
+
+// statically desplaying in terminal
 const Test = require('./modules/test.controller')
 Test.solution();
